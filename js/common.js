8987ae6c9f90f50668290a99d8d41876d2a64b77
ferbal <ferbal2000@gmail.com>
2023-10-22 09:16:03 -0300

    Correccion carga header y footer

--- js/common.js
+++ js/common.js
@@ -1,12 +1,17 @@
+
 $(function(){
-    activateSection()
+    $("header").load("includes/header.html")
+    $("footer").load("includes/footer.html")
+
+    setTimeout(function() {
+        activeSection()
+    }, 300);
+
+
 });
 
-$("header").load("includes/header.html")
-$("footer").load("includes/footer.html")
 
-
-function activateSection() {
+function activeSection() {
     let active = $('body').attr('id')
     var link = $('li.'+active+ ' a')
     $(link).addClass('active')
